b9f24f05eb6d48b105c4abed3d45fdc3b38edeb2
mekhiw301 <mekhiwilliams2001@gmail.com>
2025-02-21 17:53:48 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-# Put your app in here.
-from flask import Flask, request
-from operations import add, sub, mult, div
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/add')
-def do_add():
-    """Add a and b from URL query parameters and return result."""
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    return str(add(a, b))
+@app.route('/welcome')
+def welcome():
+    return "Welcome!"
 
-@app.route('/sub')
-def do_sub():
-    """Subtract b from a and return result."""
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    return str(sub(a, b))
+@app.route('/welcome/home')
+def welcome_home():
+    return "Welcome home!"
 
-@app.route('/mult')
-def do_mult():
-    """Multiply a and b and return result."""
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    return str(mult(a, b))
-
-@app.route('/div')
-def do_div():
-    """Divide a by b and return result."""
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    return str(div(a, b))
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome back!"
 
 if __name__ == '__main__':
     app.run(debug=True)
